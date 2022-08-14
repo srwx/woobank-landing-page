4436f7a56f1c974a9df4bc6b1074a2b2961e8b9b
Sorawong L <Sorawong2543@gmail.com>
2022-08-14 17:19:35 +0700

    edit tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // TODO: tailwind jit mode (improve performance)
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      // ถ้าไว้ข้างนอก extend พวก bg-red-500 (theme เดิมของ tailwind) จะใช้ไม่ได้ เราต้องกำหนด bg-red ขึ้นมาเอง
+      colors: {
+        primary: "#00040f",
+        secondary: "#00f6ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
+      },
+    },
+    screens: {
+      sm: "768px",
+      md: "1024px",
+      lg: "1440px",
+    },
   },
   plugins: [],
-};
+}
